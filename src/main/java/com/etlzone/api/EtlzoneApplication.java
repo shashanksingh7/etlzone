25990da09eba285bf9ab08834dc6b0f3a63fcb91
shashanksingh7 <shashanksngh082@gmail.com>
2022-11-27 19:56:21 +0530

    written by shashank

---
+++ src/main/java/com/etlzone/api/EtlzoneApplication.java
@@ -0,0 +1,13 @@
+package com.etlzone.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtlzoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtlzoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/etlzone/api/EtlzoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etlzone.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtlzoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
